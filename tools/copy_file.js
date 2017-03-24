d44f25e2ba49ff01502837a15944588db3da928d
chai2010 <chaishushan@gmail.com>
2017-03-24 23:16:43 +0800

    tools: avoid es6 syntax

--- tools/copy_file.js
+++ tools/copy_file.js
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-const fs = require('fs');
+var fs = require('fs');
 
 if(require.main === module) {
-	let args = process.argv.splice(2)
+	var args = process.argv.splice(2)
 	if(args.length != 2) {
 		console.log('usage: node copy_file.js infile outfile')
 		process.exit(1)
