d6b357c5215743b7749ae46fed123013b600f0b1
chai2010 <chaishushan@gmail.com>
2017-03-24 16:50:08 +0800

    add unit test

---
+++ test.js
@@ -0,0 +1,12 @@
+// Copyright 2017 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+let pkg = require('./index.js')
+
+exports.testVersion = function(t) {
+	t.ok(pkg.version == '1.0.1')
+	t.ok(/^\d+\.\d+\.\d+$/.test(pkg.version))
+	t.done()
+}
+
