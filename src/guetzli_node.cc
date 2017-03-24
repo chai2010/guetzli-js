bbdbadfd17f8d503319757e35758cea3252ced67
chai2010 <chaishushan@gmail.com>
2017-03-24 09:41:51 +0800

    zz

--- src/guetzli_node.cc
+++ src/guetzli_node.cc
@@ -5,12 +5,33 @@
 #include "guetzli_capi.h"
 
 #include <node.h>
+#include <node_buffer.h>
 #include <node_object_wrap.h>
 
 static void getVersion(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	args.GetReturnValue().Set(
 		v8::String::NewFromUtf8(args.GetIsolate(), guetzli_getVersion())
 	);
+}
+
+static void printBuffer(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Local<v8::Object> bufferObj = args[0]->ToObject();
+	char* s = node::Buffer::Data(bufferObj);
+	size_t n = node::Buffer::Length(bufferObj);
+	printf("data: %s\n", s);
+	printf("len: %d\n", n);
+}
+
+static void returnBuffer(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
+
+	//char s[] = "hello";
+	//v8::MaybeLocal<v8::Object> buf = node::Buffer::New(isolate, sizeof(s));
+	//strcpy(node::Buffer::Data(buf), s
+
+	//args.GetReturnValue().Set(buf);
+
+	args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "TODO!"));
 }
 
 static void helloMethod(const v8::FunctionCallbackInfo<v8::Value>& args) {
@@ -20,6 +41,8 @@ static void helloMethod(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
 static void initModule(v8::Local<v8::Object> module) {
 	NODE_SET_METHOD(module, "getVersion", getVersion);
+	NODE_SET_METHOD(module, "printBuffer", printBuffer);
+	NODE_SET_METHOD(module, "returnBuffer", returnBuffer);
 	NODE_SET_METHOD(module, "hello", helloMethod);
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ const guetzli = require('./guetzli.node');
 
 exports.version = guetzli.getVersion()
 
+exports.printBuffer = guetzli.printBuffer
+exports.returnBuffer = guetzli.returnBuffer
+
 if(require.main === module) {
 	console.log('guetzli-' + exports.version)
+
+	const s = Buffer.from('test');
+	guetzli.printBuffer(s)
+
+	console.log('buffer:', guetzli.returnBuffer())
 }
 
