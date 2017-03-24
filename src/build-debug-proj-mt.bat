b64677a773e6a7eb1b04eb6c47fd2026a5dcea64
chai2010 <chaishushan@gmail.com>
2017-03-24 13:28:25 +0800

    fix cmake debug build

--- src/build-debug-proj-mt.bat
+++ src/build-debug-proj-mt.bat
@@ -21,9 +21,14 @@ mkdir zz_build_debug_proj_mt_tmp
 cd    zz_build_debug_proj_mt_tmp
 
 cmake ..^
+  -G "Visual Studio 14 2015 Win64"^
   -DCMAKE_BUILD_TYPE=debug^
   -DCMAKE_INSTALL_PREFIX=..^
-  -DCMAKE_EXE_LINKER_FLAGS="/MANIFEST:NO"
+  ^
+  -DCMAKE_MODULE_LINKER_FLAGS="/machine:x64"^
+  -DCMAKE_SHARED_LINKER_FLAGS="/machine:x64"^
+  -DCMAKE_STATIC_LINKER_FLAGS="/machine:x64"^
+  -DCMAKE_EXE_LINKER_FLAGS="/MACHINE:x64"
 
 :: -----------------------------------------------------------------------------
 cd ..
