a8267d0a37f48f4e0d624893fc8443dd1c71813d
chai2010 <chaishushan@gmail.com>
2017-03-24 22:48:59 +0800

    index.js: better code

--- index.js
+++ index.js
@@ -2,29 +2,80 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-const fs = require('fs')
-const guetzli = require('./guetzli.node')
+var ccapi = require('./guetzli.node')
 
-const version = guetzli.getVersion()
+var assert = require('assert');
+var utils = require('./utils.js')
 
-const minQuality = 84
-const maxQuality = 110
-const defaultQuality = 95
+exports.version = ccapi.getVersion()
 
-exports.version = version
-
-exports.minQuality = minQuality
-exports.maxQuality = maxQuality
-exports.defaultQuality = defaultQuality
+exports.minQuality = 84
+exports.maxQuality = 110
+exports.defaultQuality = 95
 
 // function(buffer, width, height, stride, quality) -> Buffer
-exports.encodeGray = guetzli.encodeGray
-exports.encodeRGB = guetzli.encodeRGB
-exports.encodeRGBA = guetzli.encodeRGBA
+exports.encodeGray = function(pix_buffer, width, height, stride, quality) {
+	assert(utils.isBuffer(pix_buffer))
+	assert((width|0) > 0 && (height|0) > 0)
+	assert((stride|0) == 0 || (stride|0) >= (width|0)*1)
+	assert((quality|0) >= this.minQuality && (quality|0) <= this.maxQuality)
+
+	return ccapi.encodeGray(pix_buffer, width, height, stride, quality)
+}
+exports.encodeRGB = function(pix_buffer, width, height, stride, quality) {
+	assert(utils.isBuffer(pix_buffer))
+	assert((width|0) > 0 && (height|0) > 0)
+	assert((stride|0) == 0 || (stride|0) >= (width|0)*3)
+	assert((quality|0) >= this.minQuality && (quality|0) <= this.maxQuality)
+
+	return ccapi.encodeRGB(pix_buffer, width, height, stride, quality)
+}
+exports.encodeRGBA = function(pix_buffer, width, height, stride, quality) {
+	assert(utils.isBuffer(pix_buffer))
+	assert((width|0) > 0 && (height|0) > 0)
+	assert((stride|0) == 0 || (stride|0) >= (width|0)*4)
+	assert((quality|0) >= this.minQuality && (quality|0) <= this.maxQuality)
+
+	return ccapi.encodeRGBA(pix_buffer, width, height, stride, quality)
+}
 
 // function(data) -> {pix, width, height, channels, depth}
-exports.decodePng24 = guetzli.decodePng24
-exports.decodePng32 = guetzli.decodePng32
+exports.decodePng24 = function(data_buffer) {
+	assert(utils.isBuffer(data_buffer))
+	assert(data_buffer.length > 0)
+
+	var m = ccapi.decodePng24(data_buffer)
+
+	assert(utils.isBuffer(m.pix))
+	assert(m.width > 0 && m.height > 0)
+	assert(m.channels > 0 && m.depth > 0)
+
+	return {
+		pix: m.pix,
+		width: m.width,
+		height: m.height,
+		channels: m.channels,
+		depth: m.depth
+	}
+}
+exports.decodePng32 = function(data_buffer) {
+	assert(utils.isBuffer(data_buffer))
+	assert(data_buffer.length > 0)
+
+	var m = ccapi.decodePng32(data_buffer)
+
+	assert(utils.isBuffer(m.pix))
+	assert(m.width > 0 && m.height > 0)
+	assert(m.channels > 0 && m.depth > 0)
+
+	return {
+		pix: m.pix,
+		width: m.width,
+		height: m.height,
+		channels: m.channels,
+		depth: m.depth
+	}
+}
 
 if(require.main === module) {
 	main(process.argv.splice(2))
@@ -32,30 +83,7 @@ if(require.main === module) {
 
 function main(args) {
 	if(args.length == 1 && args[0] == '-v') {
-		console.log('guetzli-' + exports.version)
+		console.log('guetzli-' + ccapi.getVersion())
 		process.exit(0)
 	}
-	if(args.length == 1 && args[0] == '-h') {
-		console.log('Usage: guetzli input_filename output_filename')
-		process.exit(0)
-	}
-
-	if(args.length != 2) {
-		process.exit(0)
-	}
-
-	// load png
-	let data = fs.readFileSync(args[0])
-
-	// decode png image
-	let m = guetzli.decodePng32(data)
-
-	// encode jpg image
-	let jpegData = guetzli.encodeRGBA(m.pix, m.width, m.height, 0, defaultQuality)
-
-	// save jpg
-	fs.writeFileSync(args[1], jpegData)
-
-	// OK
-	console.log('Done')
 }
