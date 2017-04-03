f6d308490fd44ad0e204de26fac304f297f67dee
chai2010 <chaishushan@gmail.com>
2017-04-03 23:02:33 +0800

    fix bin build

--- lib/guetzli-cli.ts
+++ lib/guetzli-cli.ts
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-const fs = require('fs')
-const guetzli = require('./main')
+import * as fs from 'fs'
+import * as guetzli from './guetzli'
 
 export function main() {
 	let args = process.argv.splice(2)
